436f8f8df57fc05e86f6fc69282a5b1eb3bb6e9b
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-03 16:18:07 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,13 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name, :posts, :post
 
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
   end
 
 end
